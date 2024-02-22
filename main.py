9771a0aaa4f8045741f3d2c79e12123d6cb459b8
Fernando Valderrama <danimanri05@gmail.com>
2024-02-21 18:09:04 -0600

    FDVM IDGS801 Flash

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 import forms
+from flask_wtf.csrf import CSRFProtect
+from flask import g
 
 app=Flask(__name__)
+app.secret_key='esta es mi clave secreta'
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"),404
+
+@app.before_request
+def before_request():
+    g.nombre='Mario'
+    print("before 1")
+
+@app.after_request
+def after_request(response):
+    print('after 3')
+    return response
 
 @app.route("/")
 def index():
@@ -9,6 +26,7 @@ def index():
 
 @app.route("/alumnos", methods=['GET', 'POST'])
 def alumnos():
+    print("alumno: {}".format(g.nombre))
     alumno_clase = forms.UserForm(request.form)
     nom = ""
     apa = ""
@@ -16,12 +34,16 @@ def alumnos():
     edad = None
     if request.method=='POST' and alumno_clase.validate():
         nom=alumno_clase.nombre.data
+        
         apa=alumno_clase.apaterno.data
         ama=alumno_clase.amaterno.data
         edad=alumno_clase.edad.data
         print('Nombre: {}'.format(nom))
         print('apaterno: {}'.format(apa))
         print('amaterno: {}'.format(ama))
+
+        mensaje = 'Bienvenido {}'.format(nom)
+        flash(mensaje)
     return render_template("alumnos.html", form=alumno_clase, nom=nom, apa=apa, ama=ama, edad=edad)
 
 @app.route("/maestros")
